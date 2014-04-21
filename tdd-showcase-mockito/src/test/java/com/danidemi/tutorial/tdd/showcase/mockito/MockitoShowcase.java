66fb41ecd3f488fd8e1b65f40683fa2d3cc4dd9d
danidemi <demichelis@danidemi.com>
2014-04-21 23:31:59 +0200

    tdd-showcase-mockito

---
+++ tdd-showcase-mockito/src/test/java/com/danidemi/tutorial/tdd/showcase/mockito/MockitoShowcase.java
@@ -0,0 +1,106 @@
+package com.danidemi.tutorial.tdd.showcase.mockito;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.text.SimpleDateFormat;
+import java.util.Comparator;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+public class MockitoShowcase {
+	
+	public @Rule ExpectedException asError = ExpectedException.none();
+
+	@Test
+	public void mockAnIntercase() {
+
+		Comparator<String> comparator = mock(Comparator.class);
+		
+		int compare = comparator.compare("s1", "s2");
+		
+		assertThat( compare, equalTo(0) );
+		
+	}
+	
+	@Test
+	public void mockAnInterfaceAndDefineMethods() {
+
+		Comparator<String> comparator = mock(Comparator.class);
+		
+		when(comparator.compare("s1", "s2")).thenReturn(1);
+		assertThat( comparator.compare("s1", "s2"), equalTo(1) );
+		
+		when(comparator.compare("s2", "s1")).thenReturn(-1);
+		assertThat( comparator.compare("s2", "s1"), equalTo(-1) );
+		
+		assertThat( comparator.compare("k", "K"), equalTo(0) );
+		
+	}	
+	
+	@Test
+	public void mockThrowsAnException() {
+		
+		asError.expect(IllegalArgumentException.class);
+		
+		Comparator<String> comparator = mock(Comparator.class);
+		when(comparator.compare(any(String.class), any(String.class))).thenThrow( new IllegalArgumentException() );
+		
+		comparator.compare("a", "b");
+		
+	}
+	
+	@Test
+	public void mockAComplexMethod() {
+		
+		Comparator<String> comparator = mock(Comparator.class);
+		when(comparator.compare(any(String.class), any(String.class))).then(new Answer<Integer>() {
+
+			@Override
+			public Integer answer(InvocationOnMock invocation) throws Throwable {
+				Object[] arguments = invocation.getArguments();
+				return ((String)arguments[0]).length() + ((String)arguments[1]).length(); 
+			}
+		});
+		
+		assertThat( comparator.compare("Hello", "World"), equalTo(10) );
+		
+	}
+	
+	@Test
+	public void mockMethod() {
+		
+		SimpleDateFormat mock = mock( SimpleDateFormat.class );
+		
+		doThrow( new IllegalArgumentException() )
+		.when( mock )
+		.setLenient(any(Boolean.class));
+		
+		asError.expect(IllegalArgumentException.class);
+		
+		mock.setLenient(false);
+		
+	}
+	
+	/*
+	@Test
+	public void spyClass() {
+		
+		StringTokenizer st = mock( StringTokenizer.class );
+		
+		int countTokens = st.countTokens();
+		
+		assertThat( countTokens, equalTo(3) );
+		
+	}
+	*/
+
+}
