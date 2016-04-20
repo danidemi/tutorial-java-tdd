55b4848b7b6857324b2fe58cea7beb2d38289283
danidemi <escher75@gmail.com>
2016-04-21 00:09:01 +0200

    new tests

--- tdd-fill-the-code-starting-from-test/src/main/java/com/danidemi/tutorial/tdd/calc/Calc.java
+++ tdd-fill-the-code-starting-from-test/src/main/java/com/danidemi/tutorial/tdd/calc/Calc.java
@@ -4,10 +4,25 @@ package com.danidemi.tutorial.tdd.calc;
  * Created by danidemi on 19/04/2016.
  */
 public class Calc {
+
+    String display;
+
+    Calc(){
+        display = "0";
+    }
+
     public void press(char c) {
+
+        if(display.length()==9) return;
+
+        if(display.equals("0")){
+            display = "" + c;
+        }else{
+            display = display + c;
+        }
     }
 
     public String display() {
-        return null;
+        return display;
     }
 }
--- tdd-fill-the-code-starting-from-test/src/test/java/com/danidemi/tutorial/tdd/calc/CalcTest.java
+++ tdd-fill-the-code-starting-from-test/src/test/java/com/danidemi/tutorial/tdd/calc/CalcTest.java
@@ -46,10 +46,141 @@ public class CalcTest {
 
     }
 
+    @Test
+    public void shouldDisplayNumberAsSoonItIsTyped(){
+
+        Calc calc = new Calc();
+        assertDisplayAfterKeyStrokes("1", calc, '1');
+        assertDisplayAfterKeyStrokes("11", calc, '1');
+        assertDisplayAfterKeyStrokes("112", calc, '2');
+        assertDisplayAfterKeyStrokes("1124", calc, '4');
+        assertDisplayAfterKeyStrokes("11246", calc, '6');
+        assertDisplayAfterKeyStrokes("112463", calc, '3');
+        assertDisplayAfterKeyStrokes("1124637", calc, '7');
+        assertDisplayAfterKeyStrokes("11246370", calc, '0');
+        assertDisplayAfterKeyStrokes("112463700", calc, '0');
+        assertDisplayAfterKeyStrokes("112463700", calc, '1');
+
+    }
+
+    @Test
+    public void shouldSumTwoPositiveNumbers(){
+
+        Calc calc = new Calc();
+        assertDisplayAfterKeyStrokes("1", calc, '2');
+        assertDisplayAfterKeyStrokes("1", calc, '+');
+        assertDisplayAfterKeyStrokes("3", calc, '3');
+        assertDisplayAfterKeyStrokes("5", calc, '=');
+
+    }
+
+    @Test
+    public void shouldOverflowIfTheResultIsToBigToFitTheDisplay(){
+
+        Calc calc = new Calc();
+        assertDisplayAfterKeyStrokes("888888888", calc, '8','8','8','8','8','8','8','8','8');
+        assertDisplayAfterKeyStrokes("888888888", calc, '+');
+        assertDisplayAfterKeyStrokes("666666666", calc, '6','6','6','6','6','6','6','6','6');
+        assertDisplayAfterKeyStrokes("E", calc, '=');
+
+    }
+
+    @Test
+    public void shouldNotAcceptAnyKeystrokeAfterOverflow(){
+
+        Calc calc = new Calc();
+
+        goOverflowWithASum(calc);
+
+        assertDisplayAfterKeyStrokes("E", calc, '1');
+        assertDisplayAfterKeyStrokes("E", calc, '=');
+        assertDisplayAfterKeyStrokes("E", calc, '+');
+
+    }
+
+    @Test
+    public void shouldClearTheOverflow(){
+
+        Calc calc = new Calc();
+
+        goOverflowWithASum(calc);
+
+        assertDisplayAfterKeyStrokes("0", calc, 'C');
+        assertDisplayAfterKeyStrokes("1", calc, '1');
+        assertDisplayAfterKeyStrokes("1", calc, '+');
+        assertDisplayAfterKeyStrokes("2", calc, '2');
+        assertDisplayAfterKeyStrokes("3", calc, '=');
+
+    }
+
+    @Test
+    public void shouldApplyLastSumWithMultipleEqual() {
+
+        Calc calc = new Calc();
+        assertDisplayAfterKeyStrokes("3", calc, '3');
+        assertDisplayAfterKeyStrokes("3", calc, '+');
+        assertDisplayAfterKeyStrokes("22", calc, '2','2');
+        assertDisplayAfterKeyStrokes("25", calc, '=');
+        assertDisplayAfterKeyStrokes("47", calc, '=');
+        assertDisplayAfterKeyStrokes("69", calc, '=');
+        assertDisplayAfterKeyStrokes("91", calc, '=');
+
+    }
+
+    @Test
+    public void shouldSubtract() {
+
+        Calc calc = new Calc();
+        assertDisplayAfterKeyStrokes("3", calc, '3');
+        assertDisplayAfterKeyStrokes("3", calc, '-');
+        assertDisplayAfterKeyStrokes("2", calc, '2');
+        assertDisplayAfterKeyStrokes("1", calc, '=');
+
+    }
+
+    @Test
+    public void shouldKeepOnSubtract() {
+
+        Calc calc = new Calc();
+        assertDisplayAfterKeyStrokes("12", calc, '1', '2');
+        assertDisplayAfterKeyStrokes("12", calc, '-');
+        assertDisplayAfterKeyStrokes("5", calc, '5');
+        assertDisplayAfterKeyStrokes("7", calc, '=');
+        assertDisplayAfterKeyStrokes("2", calc, '=');
+        assertDisplayAfterKeyStrokes("-3", calc, '=');
+        assertDisplayAfterKeyStrokes("-8", calc, '=');
+
+    }
+
+    private void goOverflowWithASum(Calc calc1) {
+        calc1.press('9');
+        calc1.press('9');
+        calc1.press('9');
+        calc1.press('9');
+        calc1.press('9');
+        calc1.press('9');
+        calc1.press('9');
+        calc1.press('9');
+        calc1.press('9');
+        calc1.press('+');
+        calc1.press('9');
+        calc1.press('9');
+        calc1.press('9');
+        calc1.press('9');
+        calc1.press('9');
+        calc1.press('9');
+        calc1.press('9');
+        calc1.press('9');
+        calc1.press('9');
+        calc1.press('=');
+    }
+
+
     private void assertDisplayAfterKeyStrokes(String display, Calc calc, Character... keyStrokes) {
 
         Arrays.asList(keyStrokes).forEach( ks -> calc.press(ks) );
         assertThat( calc.display(), equalTo(display) );
+
     }
 
 }
